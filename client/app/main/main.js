1703b00dc32b1df9815aa76c4e30a24192f66b63
Eli Sagy <sagyeli@gmail.com>
2016-01-12 22:11:47 +0200

    First commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pagebubbleApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
