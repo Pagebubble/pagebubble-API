b1a37948a64340a1899c9ac60c6e5a96ebaac721
Eli Sagy <sagyeli@gmail.com>
2016-01-14 16:58:32 +0200

    Notification endpoint

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,26 @@
 
 angular.module('pagebubbleApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.notifications = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/notifications').success(function(notifications) {
+      $scope.notifications = notifications;
+      socket.syncUpdates('notification', $scope.notifications);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addNotification = function() {
+      if($scope.newNotification === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/notifications', { name: $scope.newNotification });
+      $scope.newNotification = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteNotification = function(notification) {
+      $http.delete('/api/notifications/' + notification._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('notification');
     });
   });
--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -18,7 +18,8 @@ var UserSchema = new Schema({
   facebook: {},
   twitter: {},
   google: {},
-  github: {}
+  github: {},
+  active: { type: Boolean, default: true }
 });
 
 /**
