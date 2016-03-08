d952a506e3b0eaf1de45deda4d6e24a7caf14535
Eli Sagy <sagyeli@gmail.com>
2016-03-08 22:05:51 +0000

    Some adjustments for https requests on production

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var mongoose = require('mongoose');
+var fs = require('fs');
 var config = require('./config/environment');
 
 // Connect to database
@@ -23,7 +24,12 @@ if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
-var server = require('http').createServer(app);
+if (config.env !== 'production') {
+  var server = require('http').createServer(app);
+}
+else {
+  var server = require('https').createServer({ key: fs.readFileSync('../pagebubble.key', 'utf8'), cert: fs.readFileSync('../pagebubble_com.crt', 'utf8'), passphrase: fs.readFileSync('../passphrase', 'utf8') }, app);
+}
 var socketio = require('socket.io')(server, {
   serveClient: config.env !== 'production',
   path: '/socket.io-client'
@@ -33,9 +39,18 @@ require('./config/express')(app);
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+server.listen(config.env !== 'production' ? config.port : 443, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.env !== 'production' ? config.port : 443, app.get('env'));
 });
+
+if (config.env === 'production') {
+  require('http')
+    .createServer(function (req, res) {
+      res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+      res.end();
+    })
+    .listen(config.port, config.ip);
+}
 
 // Expose app
 exports = module.exports = app;
